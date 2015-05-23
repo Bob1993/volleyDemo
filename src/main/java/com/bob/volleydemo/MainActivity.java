d55038923a72a57fb18627d125e5fac88da5cba2
Bob <bob1993.dev@gmail.com>
2015-05-23 23:46:41 +0800

    引入volley框架，完成StringRequest和JsonObjectRequest

--- src/main/java/com/bob/volleydemo/MainActivity.java
+++ src/main/java/com/bob/volleydemo/MainActivity.java
@@ -2,16 +2,54 @@ package com.bob.volleydemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private RequestQueue mQueue;//创建一个请求队列
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mQueue= Volley.newRequestQueue(this);
+        StringRequest stringRequest= new StringRequest("http://www.baidu.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Log.i("Tag",s);
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.i("Tag", volleyError.getMessage(), volleyError);
+            }
+        });//默认的是Get
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,"http://m.weather.com.cn/data/101010100.html", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+
+        mQueue.add(stringRequest);
+        mQueue.add(jsonObjectRequest);
+
+    }
 }
