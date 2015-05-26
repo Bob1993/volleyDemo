9e29c46bc721ff083705f030119f31052866db9d
Bob <bob1993.dev@gmail.com>
2015-05-26 17:34:49 +0800

    添加ImageLoader，NetWorkImageView两个demo

--- src/main/java/com/bob/volleydemo/MainActivity.java
+++ src/main/java/com/bob/volleydemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.LruCache;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,10 +13,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.*;
 
 import org.json.JSONObject;
 
@@ -28,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private StringRequest stringRequest;
     private JsonObjectRequest jsonObjectRequest;
     private ImageRequest imageRequest;
-
+    private ImageLoader imageLoader;
+    private NetworkImageView networkImageView;
 
 
     @Override
@@ -42,14 +41,15 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageReques(editText.getText().toString());
-                mQueue.add(imageRequest);
+              //  imageReques(editText.getText().toString());
+                imageLoade(editText.getText().toString());//内部也是ImageRequest实现方式
+                //mQueue.add(imageRequest);
             }
         });
 
         stringReques();
         jsonObjectReques();
-
+        networkImage();
 
         mQueue.add(stringRequest);
         mQueue.add(jsonObjectRequest);
@@ -97,8 +97,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e("TAG", error.getMessage(), error);
-                imageView.setImageResource(R.mipmap.default_image);
-            }
+                imageView.setImageResource(R.mipmap.failed_image);
+            }//两个0参数表示允许图片的最大宽高，0表示不压缩
         });
     }
+
+    public void imageLoade(String url){//缓存图片，过滤掉重复url链接
+        imageLoader= new ImageLoader(mQueue, new ImageLoader.ImageCache() {//匿名内部类实现ImageCache接口
+            int maxSize = 10 * 1024 * 1024;//10mb
+            LruCache<String, Bitmap> mCache= new LruCache<String,Bitmap>(maxSize){//缓存总大小
+                @Override//匿名内部类继承LruCache类，即缓存池
+                protected int sizeOf(String key, Bitmap value) {
+                    return value.getRowBytes()*value.getHeight();//返回图片大小
+                }
+            };
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);//将图片存入缓存
+            }
+        });
+
+        ImageLoader.ImageListener listener= imageLoader.getImageListener(imageView,R.mipmap.default_imaging,R.mipmap.failed_image);
+        imageLoader.get(url,listener);
+    }
+
+    public void networkImage(){
+        imageLoader= new ImageLoader(mQueue, new ImageLoader.ImageCache() {
+            @Override
+            public Bitmap getBitmap(String s) {
+                return null;
+            }
+
+            @Override
+            public void putBitmap(String s, Bitmap bitmap) {
+
+            }
+        });
+        networkImageView= (NetworkImageView) findViewById(R.id.img_network);
+        networkImageView.setDefaultImageResId(R.mipmap.default_imaging);
+        networkImageView.setErrorImageResId(R.mipmap.failed_image);
+        networkImageView.setImageUrl("http://img.my.csdn.net/uploads/201404/13/1397393290_5765.jpeg",
+                imageLoader);
+    }
+
+
 }
